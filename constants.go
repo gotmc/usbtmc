fdfbe88bcd751eafbdee7ee5a2884415b57c5007
Matthew Rankin <matthew@cumulusware.com>
2016-01-15 15:15:21 -0600

    Remove unnecessary constants

--- constants.go
+++ constants.go
@@ -15,16 +15,10 @@ const headerSize = 12
 // The base class codes are part of the USB class codes "used to identify a
 // device's functionality and to nominally load a device driver based on that
 // functionality." [Source: http://www.usb.org/developers/defined_class]
-// The USBTMC standard refers to these as the bInterfaceClass.
+// The USBTMC standard refers to these as the bInterfaceClass. The only base
+// class code required for USBTMC is the Application Specific Base Class 0xFE.
 const (
-	audioBaseClass               bInterfaceClass = 0x01
-	communicationsBaseClass      bInterfaceClass = 0x02
-	hidBaseClass                 bInterfaceClass = 0x03
-	physicalBaseClass            bInterfaceClass = 0x05
-	imageBaseCalss               bInterfaceClass = 0x06
-	misellaneousBaseClass        bInterfaceClass = 0xef
 	applicationSpecificBaseClass bInterfaceClass = 0xfe
-	vendorSpecificBaseClass      bInterfaceClass = 0xff
 )
 
 type bInterfaceSubClass byte
