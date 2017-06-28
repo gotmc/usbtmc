acabdb022c314634319f49c718a3b011ee4b642c
Matthew Rankin <matthew@cumulusware.com>
2017-06-28 09:56:11 -0500

    First attempt at libusb driver: not working

---
+++ driver/libusb/context.go
@@ -0,0 +1,79 @@
+// Copyright (c) 2015-2017 The usbtmc developers. All rights reserved.
+// Project site: https://github.com/gotmc/usbtmc
+// Use of this source code is governed by a MIT-style license that
+// can be found in the LICENSE.txt file for the project.
+
+package libusb
+
+import (
+	"fmt"
+
+	"github.com/gotmc/libusb"
+	"github.com/gotmc/usbtmc"
+	"github.com/gotmc/usbtmc/driver"
+)
+
+// Driver implements the visa.Driver interface required by usbtmc using the
+// github.com/gotmc/libusb libusb driver.
+type Driver struct {
+}
+
+func init() {
+	usbtmc.Register(&Driver{})
+}
+
+type libusbContext struct {
+	ctx *libusb.Context
+}
+
+// NewContext creates a new libusb session/context.
+func (d Driver) NewContext() (driver.Context, error) {
+	var c libusbContext
+	ctx, err := libusb.NewContext()
+	c.ctx = ctx
+	return &c, err
+}
+
+// Close the libusb session/context.
+func (c *libusbContext) Close() error {
+	return c.ctx.Close()
+}
+
+// NewDeviceByVIDPID creates new USB device based on the given the
+// vendor ID and product ID. If multiple USB devices matching the VID and PID
+// are found, only the first is returned.
+func (c *libusbContext) NewDeviceByVIDPID(VID, PID uint) (driver.USBDevice, error) {
+	dev, dh, err := c.ctx.OpenDeviceWithVendorProduct(uint16(VID), uint16(PID))
+	if err != nil {
+		return nil, err
+	}
+	usbDeviceDescriptor, _ := dev.GetDeviceDescriptor()
+	if err != nil {
+		return nil, err
+	}
+	defer dh.Close()
+	configDescriptor, err := dev.GetActiveConfigDescriptor()
+	if err != nil {
+		return nil, fmt.Errorf("failed getting active config: %s", err)
+	}
+	firstDescriptor := configDescriptor.SupportedInterfaces[0].InterfaceDescriptors[0]
+	err = dh.ClaimInterface(0)
+	if err != nil {
+		return nil, fmt.Errorf("error claiming USB interface: %s", err)
+	}
+	bulkOutput := firstDescriptor.EndpointDescriptors[0]
+	bulkInput := firstDescriptor.EndpointDescriptors[1]
+	interruptEndpoint := firstDescriptor.EndpointDescriptors[2]
+
+	d := Device{
+		Timeout:           2000,
+		USBDevice:         dev,
+		DeviceDescriptor:  usbDeviceDescriptor,
+		DeviceHandle:      dh,
+		ConfigDescriptor:  configDescriptor,
+		BulkInEndpoint:    bulkInput,
+		BulkOutEndpoint:   bulkOutput,
+		InterruptEndpoint: interruptEndpoint,
+	}
+	return &d, nil
+}
--- examples/fgen/main.go
+++ examples/fgen/main.go
@@ -14,6 +14,7 @@ import (
 
 	"github.com/gotmc/usbtmc"
 	_ "github.com/gotmc/usbtmc/driver/truveris"
+	// _ "github.com/gotmc/usbtmc/driver/libusb"
 )
 
 func main() {
